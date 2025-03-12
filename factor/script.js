089751b1352a48f414cda4844f43d79d3341ddff
sobhanhk <sobhan.hk@outlook.com>
2025-03-12 15:46:42 +0330

    fix footer overflow and fix button reset functionality

--- factor/script.js
+++ factor/script.js
@@ -41,3 +41,82 @@ document.getElementById("add-one-row").addEventListener("click", function (e) {
 </tr>`
   );
 });
+
+document.getElementById("reset").addEventListener("click", function (e) {
+  document.getElementById("table").innerHTML = `
+          <tbody id="table">
+            <tr>
+              <td>1</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-1" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-1" /></td>
+              <td><label id="total-1" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-2" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-2" /></td>
+              <td><label id="total-2" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-3" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-3" /></td>
+              <td><label id="total-3" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-4" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-4" /></td>
+              <td><label id="total-4" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-5" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-5" /></td>
+              <td><label id="total-5" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-6" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-6" /></td>
+              <td><label id="total-6" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-7" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-7" /></td>
+              <td><label id="total-7" class="total">0</label></td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td><input type="text" class="product-name" /></td>
+              <td>
+                <input type="number" class="product-quantity" id="quantity-8" />
+              </td>
+              <td><input type="number" class="product-price" id="fi-8" /></td>
+              <td><label id="total-8" class="total">0</label></td>
+            </tr>
+          </tbody>`;
+  document.getElementById("total-price").textContent = 0;
+});
